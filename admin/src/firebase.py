962213c46c6494c3299145fd2a39687b3d769015
TBurn2 <43250044+TBurn2@users.noreply.github.com>
2026-08-21 20:37:05 +0000

    added in button for firebase connection

---
+++ admin/src/firebase.py
@@ -0,0 +1,7 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate('path/to/serviceAccount.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
--- admin/src/main.py
+++ admin/src/main.py
@@ -4,16 +4,23 @@ import folium
 from streamlit_folium import st_folium
 st.title("diving app page")
 
-site_name = st.text_input("Site location")
-site_address = st.text_area("Address")
-geopoint = st.text_input("Geopoint")
+with st.form("my_form"):
+    st.write("Inside the form")
+    site_name = st.text_input("Site Name")
+    site_address = st.text_area("Address")
 
-m = folium.Map(location=[51.5074, -0.1278], zoom_start=10)
-m.add_child(folium.LatLngPopup())  # shows lat/lng in a popup on click
 
-map_data = st_folium(m, width=700, height=500)
+    m = folium.Map(location=[51.5074, -0.1278], zoom_start=10)
+    m.add_child(folium.LatLngPopup())  # shows lat/lng in a popup on click
 
-if map_data and map_data.get("last_clicked"):
-    lat = map_data["last_clicked"]["lat"]
-    lng = map_data["last_clicked"]["lng"]
-    st.write(f"You clicked: {lat}, {lng}")
+    map_data = st_folium(m, width=700, height=500)
+
+    if map_data and map_data.get("last_clicked"):
+        lat = map_data["last_clicked"]["lat"]
+        lng = map_data["last_clicked"]["lng"]
+        st.write(f"You clicked: {lat}, {lng}")
+
+    submitted = st.form_submit_button("Submit")
+    if submitted:
+        st.write("slider", site_name, "checkbox", site_address)
+st.write("Outside the form")
