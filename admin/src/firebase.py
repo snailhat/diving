3a00cd16b8294a010cb9d3b56dd4836b459d8372
TBurn2 <43250044+TBurn2@users.noreply.github.com>
2026-08-21 22:11:22 +0000

    added a new dive location function

--- admin/src/firebase.py
+++ admin/src/firebase.py
@@ -1,7 +1,27 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import pandas as pd
+import streamlit as st
 
-cred = credentials.Certificate('path/to/serviceAccount.json')
-app = firebase_admin.initialize_app(cred)
-db = firestore.client()
+@st.cache_resource
+def get_db(): 
+    cred = credentials.Certificate('firebase.json')
+    app = firebase_admin.initialize_app(cred)
+    return firestore.client()
+
+
+def get_locations(db):
+    
+    locations_ref = db.collection("locations")
+    docs = list(locations_ref.stream())
+
+    locations_dict = list(map(lambda x: x.to_dict(), docs))
+    df = pd.DataFrame(locations_dict)
+    
+    return df
+
+def add_location(db,site_id, site_name, site_address, lat, long):
+
+    doc_ref = db.collection("locations").document(site_id)
+    doc_ref.set({"name": site_name, "address": site_address, "geopoint": firestore.firestore.GeoPoint(lat, long) })
--- admin/src/main.py
+++ admin/src/main.py
@@ -2,10 +2,18 @@ import streamlit as st
 import pandas as pd
 import folium
 from streamlit_folium import st_folium
+from firebase import add_location, get_db, get_locations
+
+db = get_db()
+
 st.title("diving app page")
 
+df = get_locations(db)
+
+st.dataframe(df, hide_index=True)
+
 with st.form("my_form"):
-    st.write("Inside the form")
+    site_id = st.text_input("Site ID")
     site_name = st.text_input("Site Name")
     site_address = st.text_area("Address")
 
@@ -14,13 +22,12 @@ with st.form("my_form"):
     m.add_child(folium.LatLngPopup())  # shows lat/lng in a popup on click
 
     map_data = st_folium(m, width=700, height=500)
-
+    lat = 0
+    long = 0
     if map_data and map_data.get("last_clicked"):
         lat = map_data["last_clicked"]["lat"]
-        lng = map_data["last_clicked"]["lng"]
-        st.write(f"You clicked: {lat}, {lng}")
+        long = map_data["last_clicked"]["lng"]
 
     submitted = st.form_submit_button("Submit")
     if submitted:
-        st.write("slider", site_name, "checkbox", site_address)
-st.write("Outside the form")
+        add_location(db, site_id, site_name, site_address, lat, long)
